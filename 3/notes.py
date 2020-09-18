9868925485ba2df0c310be86fa937d1ce21e2d9c
Andrey <andrtazet@gmail.com>
2020-09-18 21:30:55 +0300

    1

--- 3/notes.py
+++ 3/notes.py
@@ -13,6 +13,9 @@ class Note:
             return self.dict[self.note]
         else:
             return self.note
+    
+    def get_index(self):
+        return self.notes.index(self.note)
 
     def __eq__(self, other):
         return self.note == other.note
@@ -39,10 +42,7 @@ class Note:
                 index = len(self.notes) - 1
             else:
                 index -= 1
-        if self.long:
-            return self.dict[self.notes[index]]
-        else:
-            return self.notes[index]
+        return Note(self.notes[index], is_long=self.long)
 
     def __rshift__(self, other):
         index = self.notes.index(self.note)
@@ -51,13 +51,57 @@ class Note:
                 index = 0
             else:
                 index += 1
-        if self.long:
-            return self.dict[self.notes[index]]
-        else:
-            return self.notes[index]
+        return Note(self.notes[index], is_long=self.long)
 
     def get_interval(self, other):
         return self.intervals[abs(self.notes.index(self.note) - other.notes.index(other.note))]
+
+
+
+class Melody:
+    def __init__(self, notes=[]):
+        self.notes = notes
+    
+    def __str__(self):
+        return ', '.join([str(i) for i in self.notes]).capitalize()
+    
+    def append(self, note):
+        self.notes.append(note)
+    
+    def replace_last(self, note):
+        if len(self.notes) > 0:
+            self.notes = self.notes[:-1] + [note]
+            return None
+        self.notes = [note]
+    
+    def remove_last(self):
+        if len(self.notes) > 0:
+            self.notes = self.notes[:-1]
+    
+    def clear(self):
+        self.notes.clear()
+    
+    def __len__(self):
+        return len(self.notes)
+    
+    def __rshift__(self, other):
+        output = self.notes[:]
+        for i in range(other):
+            for l in output:
+                if l.get_index() == 6:
+                    return Melody(self.notes)
+                l >> 1
+        return Melody(output)
+    
+    def __lshift__(self, other):
+        output = self.notes[:]
+        for i in range(other):
+            for l in output:
+                if l.get_index() == 6:
+                    return Melody(self.notes)
+                l >> 1
+        return Melody(output)
+
 
 
 class LoudNote(Note):
@@ -82,8 +126,19 @@ class NoteWithOctave(Note):
         return f'{super().__str__()} ({self.octave})'
 
 
+
 N = 7
 PITCHES = ["до", "ре", "ми", "фа", "соль", "ля", "си"]
 LONG_PITCHES = ["до-о", "ре-э", "ми-и", "фа-а", "со-оль", "ля-а", "си-и"]
 INTERVALS = ["прима", "секунда", "терция",
              "кварта", "квинта", "секста", "септима"]
+
+melody = Melody([Note('ля'), Note('соль'), Note('ми'),  Note('до', True)])
+print(melody)
+print(Melody() >> 2)
+melody_up = melody >> 1
+melody_down = melody << 1
+melody.replace_last(Note('соль'))
+print('>> 1:', melody_up)
+print('<< 1:', melody_down)
+print(melody)
